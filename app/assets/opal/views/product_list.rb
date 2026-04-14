c6d253b9801bec3c43ee29c5c792c9e8235e316d
Andy Maleh <andy.am@gmail.com>
2026-04-14 18:32:49 -0400

    Add price formatting

--- app/assets/opal/views/product_list.rb
+++ app/assets/opal/views/product_list.rb
@@ -10,7 +10,7 @@ class ProductList
       h1 { 'Product List' }
       ul {
         products_attributes['products'].each do |product_attributes|
-          li { "#{product_attributes['name']} | #{product_attributes['brand']} | #{product_attributes['gender']} | #{product_attributes['age']} | #{product_attributes['price']}`" }
+          li { "#{product_attributes['name']} | #{product_attributes['brand']} | #{product_attributes['gender']} | #{product_attributes['age']} | $#{'%.2f' % product_attributes['price']}" }
         end
       }
     }
